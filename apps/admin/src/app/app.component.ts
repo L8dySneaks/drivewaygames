78b0cdd1741439ec536b6f41b4f9b9f6df369d90
Steph Symosko <steph.symosko@gmail.com>
2020-12-13 16:42:38 -0500

    initial commit

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'drivewaygames-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
